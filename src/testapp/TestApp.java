e93c386910ad48146c9b09dc6d517070dce15522
Robert <Robert@130.243.43.25>
2015-01-29 15:38:41 +0100

    test

---
+++ src/testapp/TestApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author Robert
+ */
+public class TestApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String hej = "Nummer är: ";
+       int i = 1;
+       
+        for (int j = 0; j < 10; j++) {
+            System.out.println(hej + i);
+            i++;
+        }
+    }
+    
+    
+    
+}
